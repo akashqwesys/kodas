0ee3ab1c460ab4db60947d3d2d819f60d6726235
vasimlook <vasimlook@gmail.com>
2022-02-08 16:26:33 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -144,6 +144,7 @@ $active_group = 'default';
 
 $query_builder = TRUE;
 
+if($_SERVER['HTTP_HOST']=='localhost'){
 $db['default'] = array(
 
 	'dsn' => '',
@@ -185,3 +186,46 @@ $db['default'] = array(
 	'save_queries' => TRUE,
 
 );
+}else{
+	$db['default'] = array(
+
+		'dsn' => '',
+	
+		'hostname' => 'localhost',
+	
+		'username' => 'akashs_qkodas',
+	
+		'password' => '4Nhwnp15=H@m',
+	
+		'database' => 'akashs_qkodas',
+	
+		'dbdriver' => 'mysqli',
+	
+		'dbprefix' => '',
+	
+		'pconnect' => FALSE,
+	
+		'db_debug' => (ENVIRONMENT !== 'production'),
+	
+		'cache_on' => FALSE,
+	
+		'cachedir' => '',
+	
+		'char_set' => 'utf8',
+	
+		'dbcollat' => 'utf8_general_ci',
+	
+		'swap_pre' => '',
+	
+		'encrypt' => FALSE,
+	
+		'compress' => FALSE,
+	
+		'stricton' => FALSE,
+	
+		'failover' => array(),
+	
+		'save_queries' => TRUE,
+	
+	);
+}
